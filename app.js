3862e12542789315238b2798aeff014c3054d630
xemmefxMFx <femmemexxx@gmail.com>
2022-10-26 03:08:21 +0700

    Add clear function with Calculator class

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
+        // clearing the calculator upon creation
+        this.clear()
     }
 
     clear() {
-
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
     }
 
     delete() {
@@ -28,6 +32,7 @@ class Calculator {
     updateDisplay() {
 
     }
+
 }
 
 
