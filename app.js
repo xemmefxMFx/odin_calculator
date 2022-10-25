8f964cc483a2f1865e264c64b88f19d04b55a018
xemmefxMFx <femmemexxx@gmail.com>
2022-10-26 02:43:36 +0700

    Add Class and Declare functions
    Class Calculator. Require according all clear, delete, append number in a string format, choose operation through switch case, compute number based on operation, and return calculation result through updating the display.

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+class Calculator {
+    // initializing object within the class
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+    }
+
+    clear() {
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
+}
+
+
+
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
